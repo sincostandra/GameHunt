dcdd76b04343f00b3c86a5c4ca12069a319a208b
Vincent-Davis <leonardvincent62@gmail.com>
2024-10-14 12:15:43 +0700

    css implemented

--- search/forms.py
+++ search/forms.py
@@ -1,7 +1,9 @@
-from django.forms import ModelForm
+from django import forms
 from search.models import Game
 
-class GameForm(ModelForm):
+
+
+class GameForm(forms.ModelForm):
     class Meta:
         model = Game
         fields = [
@@ -10,3 +12,59 @@ class GameForm(ModelForm):
             "toko1", "alamat1", "toko2", "alamat2", 
             "toko3", "alamat3"
         ]
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter game name'
+            }),
+            'year': forms.NumberInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter release year'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'form-input', 
+                'rows': 3, 
+                'placeholder': 'Enter game description'
+            }),
+            'developer': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter developer name'
+            }),
+            'genre': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter game genre'
+            }),
+            'ratings': forms.NumberInput(attrs={
+                'class': 'form-input', 
+                'step': 0.1, 'min': 0, 'max': 5,
+                'placeholder': 'Enter ratings (0-5)'
+            }),
+            'harga': forms.NumberInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter price in IDR'
+            }),
+            'toko1': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter first store'
+            }),
+            'alamat1': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter address of first store'
+            }),
+            'toko2': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter second store (optional)'
+            }),
+            'alamat2': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter address of second store (optional)'
+            }),
+            'toko3': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter third store (optional)'
+            }),
+            'alamat3': forms.TextInput(attrs={
+                'class': 'form-input', 
+                'placeholder': 'Enter address of third store (optional)'
+            }),
+        }
