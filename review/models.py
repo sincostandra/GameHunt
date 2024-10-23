37a32196782094babeed80064be433730615f4ff
sincostandra <utandraemail@gmail.com>
2024-10-23 16:28:32 +0700

    feat: model create

--- review/models.py
+++ review/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+# from search.game import Game
+class Review(models.Model):
+    judul_review = models.CharField(max_length=255)
+    teks_review = models.TextField()
+    nilai_review = models.IntegerField()
+    pengguna = models.ForeignKey(User, on_delete=models.CASCADE)
+    # game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    tanggal_review = models.DateTimeField(auto_now_add=True)
--- review/views.py
+++ review/views.py
@@ -1,3 +1,23 @@
+import json
 from django.shortcuts import render
+from review.models import review 
 
+from review.models import Review
+# from search.models import Game
+
+# from books.models import Book
+# from reviews.forms import ReviewForm
+
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound, JsonResponse
+from django.core import serializers
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 # Create your views here.
+
+@login_required(login_url='main:login')
+def show_reviews(request, game_id):
+    game = Game.objects.get(pk=game_id)
+    
