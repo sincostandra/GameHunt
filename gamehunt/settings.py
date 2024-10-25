eae2093f4dbeaf1a3833c90552cf9f14acd1edea
oscarryanda <scarryanda@gmail.com>
2024-10-25 23:25:54 +0700

    feat: authentication and base userprofile implemented

--- gamehunt/settings.py
+++ gamehunt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!tjgo5u1-1s=d2_*ikvi*l0p9ol+=z^48qqvuwyw9_+l@_q_cc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "utandra-nur-gamehunt.pbp.cs.ui.ac.id"]
 
